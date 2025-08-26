b393eb7167e218bd784a6f382bfce63cae620c6a
aqillaahmad <aqillaahmad2009@gmail.com>
2025-08-26 20:17:27 +0700

    Add files via upload

---
+++ latihan1/latihan2.php
@@ -0,0 +1,12 @@
+<?php
+// Ini adalah komentar satu baris
+/* Ini adalah komentar yang bisa menjelaskan
+banyak baris */
+/**
+* Fungsi ini menampilkan pesan
+* @param string $message
+*/
+function displayMessage($message) {
+echo $message;
+}
+?>
---
+++ latihan1/latihan8.php
@@ -0,0 +1,17 @@
+<?php
+for ($i = 1; $i <= 5; $i++) {
+echo "Angka: $i <br>";
+}
+?>
+<?php
+for ($i = 1; $i <= 5; $i++) {
+echo "Angka: $i <br>";
+}
+?>
+<?php
+$buah = array("apel", "jeruk",
+"mangga");
+foreach ($buah as $item) {
+echo $item . "<br>";
+}
+?>
